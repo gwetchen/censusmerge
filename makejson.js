7c77516c4c8feae44ba6d3af47ad0caeb67cf3ce
gretchen <gwetchenn@gmail.com>
2023-09-12 09:32:03 +0200

    json export working

--- makejson.js
+++ makejson.js
@@ -3,35 +3,53 @@ import fetch from 'node-fetch'
 globalThis.fetch = fetch;
 import * as fs from 'fs';
 
-export default class DataLoaderService {
 
 
-    rawDataUri = '/data/CensusPlusTurtle.lua';
+
+
+//var rawDataUri = '/CensusPlusTurtle.lua';
 //const raw = fetch(rawDataUri);
 //const json = LuaParse.parse(raw, {
 //    comments: false,
 //    });
 //fs.writeFile('output.json', json);
 
+function _parseLua(input) {
+    let result = LuaParse.parse(input, {
+        comments: false,
+    });
+    return result;
+}
 
 
-async load() {
-    let raw = await fetch(this.rawDataUri)
-    let json = this._parseLua(raw);
-    fs.writeFile("output.json", json, (err) => {
+async function load() {
+    let raw = fs.readFileSync('CensusPlusTurtle.lua', 'utf8');
+    let ast = _parseLua(raw);
+    fs.writeFile("output.json", JSON.stringify(ast), (err) => {
         if (err)
             console.log(err);
         else {
             console.log("File written successfully");
           }
     });
-    return json;
-}
 
-_parseLua(input) {
-    let result = LuaParse.parse(input, {
-        comments: false,
+}
+load();
+
+async function load1() {
+    let raw = fs.readFileSync('CensusPlusTurtle1.lua', 'utf8');
+    let ast = _parseLua(raw);
+    fs.writeFile("output1.json", JSON.stringify(ast), (err) => {
+        if (err)
+            console.log(err);
+        else {
+            console.log("File written successfully");
+          }
     });
-    return result;
+
 }
-}
+load1();
+
+
+
+;
